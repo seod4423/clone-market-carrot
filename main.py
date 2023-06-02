2a2104d73cab1389f96ed647cb6c66d4b581c5f5
seod4423 <seod4423@gmail.com>
2023-06-02 23:09:30 +0900

    [당근클론]  배포 준비
    -DB 준비
    -배포 서버 세팅
      python환경에 맞춰 이미지 준비

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import sqlite3
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                image BLOB,
+                price INTEGER NOT NULL,
+                description TEXT,
+                place TEXT NOT NULL,
+                createAt TEXT NOT NULL
+            );
+            """)
+
 app = FastAPI()
 
 @app.post("/items")
@@ -41,7 +53,7 @@ async def get_image(item_id):
     res = cur.execute(f"""
                       SELECT image FROM items WHERE id={item_id}
                       """).fetchone()[0]
-    return Response(content=bytes.fromhex(res))
+    return Response(content=bytes.fromhex(res), media_type='image/*')
 
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
